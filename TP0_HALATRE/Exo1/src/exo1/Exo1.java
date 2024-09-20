0554114531f9067810f84513259a9c90540f9af7
ennio <ennio@localhost>
2024-09-20 18:42:37 +0200

    Fin Exo 1

---
+++ TP0_HALATRE/Exo1/src/exo1/Exo1.java
@@ -0,0 +1,28 @@
+/*Ennio Halatre
+ * TP B
+ * Rôle
+ *20/09
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ennio
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom = sc.nextLine();
+    }
+    
+}
