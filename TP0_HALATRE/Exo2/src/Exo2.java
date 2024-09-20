7c8c028f7d3bb60073097f39b64deb055abee27f
ennio <ennio@localhost>
2024-09-20 18:43:13 +0200

    Fin Exo 2

---
+++ TP0_HALATRE/Exo2/src/Exo2.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+/*
+*Halatre Ennio
+*TD B 
+*20/09
+package exo2;
+
+/**
+ *
+ * @author ennio
+ */
+public class Exo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        //Declaration des variables
+        int nb; // nombre dentiers a additionner
+        int result; // resultat
+        int ind; //indice
+        nb=5;
+        result=0;   
+       
+        int nb;
+        //nb=5;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer le nombre :");
+        nb=sc.nextInt(); // On demande a sc de donner le prochain entier
+
+        // Addition des nb premiers entiers 
+        ind=1;
+        while (ind <= nb) {
+            result=result+ind;
+            ind = ind +1 ;
+        }
+
+        // Affichage du resultat
+        System.out.println();
+        System.out.println("La somme des "+ nb + "entiers est: "+result);
+    }
+    
+}
