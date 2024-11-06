e87262e6ba69dfb7444c57f27bb158b8f1c31fb3
ennio <ennio@localhost>
2024-11-06 13:40:33 +0100

    TP2 relation HALATRE

--- TP2_relation_1_HALATRE/src/tp2_relation_1_halatre/TP2_relation_1_HALATRE.java
+++ TP2_relation_1_HALATRE/src/tp2_relation_1_halatre/TP2_relation_1_HALATRE.java
@@ -22,12 +22,26 @@ public class TP2_relation_1_HALATRE {
         Personne bob = new Personne("Bobby", "Sixkiller");
         Personne reno = new Personne("Reno", "Raines");
         System.out.println("liste des voitures disponibles "+ uneClio + "\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
-        bob.liste_voitures[0] = uneClio ;
-        bob.nbVoitures = 1 ;
         
+        bob.liste_voitures[0] = uneClio ;
+        bob.nb_Voitures = 1 ;
         uneClio.ProprietaireVoiture = bob ;
         System.out.println("la premiere voiture de Bob est " + bob.liste_voitures[0] ) ;
-        
+        bob.liste_voitures[1] = une2008 ;
+
+        bob.nb_Voitures = 2 ;
+        une2008.proprietaire = bob ;
+        System.out.println("la deuxieme voiture de Bob est " + bob.liste_voitures[1] ) ;
+      
+        reno.liste_voitures[0] = uneAutreClio ;
+        reno.nb_Voitures = 1 ;
+        uneAutreClio.proprietaire = reno ;
+        System.out.println("la premiere voiture de reno est " + reno.liste_voitures[0] ) ;
+
+        reno.liste_voitures[1] = uneMicra ;
+        reno.nb_Voitures = 2 ;
+        uneMicra.proprietaire = reno ;
+        System.out.println("la deuxieme voiture de reno est " + reno.liste_voitures[1] ) ;
     }
     
 }
